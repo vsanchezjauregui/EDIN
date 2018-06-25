883cdadd46d1e3662b9ea670da97f54a7ebc7710
vsanchezjauregui <40339144+vsanchezjauregui@users.noreply.github.com>
2018-06-25 15:42:14 -0600

    Se hicieron correcciones menores

--- controller/crear_clase.php
+++ controller/crear_clase.php
@@ -15,11 +15,10 @@ $modulo = $_GET['modulo'];
 $alumnos = $_GET['alumnos'];
 $fecha = substr($_GET['fecha'], 6)."-".substr($_GET['fecha'], 0,2)."-".substr($_GET['fecha'], 3,2);
 $horas = $_GET['horas'];
-$observaciones = $_POST["observaciones"];
+$observaciones = $_GET["observaciones"];
 //defino los valores para insertar en la clase 
 $campos_clase = "classes.id_Open_mods, classes.class_date, classes.class_time, classes.class_observations";
 $valores_clase = "$modulo, '$fecha', $horas, '$observaciones'";
-
 //Creo la clase
 $query_insertar = "INSERT INTO classes ($campos_clase) VALUES ($valores_clase)"; 
 //echo $query_insertar."<br><br>";
@@ -41,14 +40,13 @@ if ($insertar){
 	//Inserto los alumnos asistentes en el puente
 	$query_insertar_alumnos = "INSERT INTO bridge_class_benef (bridge_class_benef.id_Beneficiaries, bridge_class_benef.id_classes) VALUES ";
 	foreach ($alumnos as $alumno) {
-		$query_insertar_alumnos .= "($alumno, $id_clase),";
+		$query_insertar_alumnos .= "($alumno, $id_clase[0]),";
 	}
 	$query_insertar_alumnos = substr($query_insertar_alumnos,0,-1);
 	//echo $query_insertar_alumnos."<br><br>";
 	$insertar = mysqli_query($con,$query_insertar_alumnos);
-	
 	if (!$insertar) {
-		$error = 2
+		$error = 2;
 	}
 
 	//////////////////////////////////////////////////////////////////////
@@ -60,22 +58,20 @@ if ($insertar){
 	//averiguo si hay participaciones
 	$query_buscar_participaciones = "SHOW TABLES LIKE 'temp_participaciones'";
 	$hayparti = $conex->consultaunica($query_buscar_participaciones, $con);
-
 	if (!is_null($hayparti)) {
-		//si hay participaciones
+		
 		$query_participaciones = "SELECT * FROM temp_participaciones";
 		$participaciones = $conex->consulta_varios($query_participaciones, $con);
 		$query_insertar_participaciones = "INSERT into bridge_class_alliance (bridge_class_alliance.id_classes, bridge_class_alliance.id_alliance, bridge_class_alliance.alliance_cooperation) VALUES ";
 		foreach ($participaciones as $participacion) {
-			$query_insertar_participaciones .= "($id_clase, ".$participacion["id_Alianza"].",'".$participacion["descripcion"]."'),";
+			$query_insertar_participaciones .= "($id_clase[0], ".$participacion["id_Alianza"].",'".$participacion["descripcion"]."'),";
 		}
 		//inserto las participaciones en la tabla correspondiente
 		$query_insertar_participaciones = substr($query_insertar_participaciones,0,-1);
 		//echo $query_insertar_participaciones."<br><br>";
-		$insertar = mysqli_query($con,$query_insertar_participacion);
-		
+		$insertar = mysqli_query($con,$query_insertar_participaciones);
 		if (!$insertar) {
-			$error = 3
+			$error = 3;
 		}
 
 		//////////////////////////////////////////////////////////////////////
@@ -108,9 +104,5 @@ switch ($error) {
         break;
 }
 
-
-
-
-
-
 ?>
+
